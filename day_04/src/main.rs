6a6db12b2bf34238f70b06ba4b1e2f2a418b227e
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2022-12-04 14:18:59 +0000

    Day 04 Part 1

---
+++ day_04/src/main.rs
@@ -0,0 +1,50 @@
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+    path::Path,
+};
+
+use itertools::Itertools;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let num_overlapping = num_overlapping_assignments(&args[1]);
+    println!("Found {num_overlapping} instances of overlapping assignments.")
+}
+
+fn num_overlapping_assignments<P: AsRef<Path>>(path: P) -> usize {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    reader
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let line = line.unwrap();
+            let bounds = line
+                .split(",")
+                .map(|assignment| {
+                    assignment
+                        .split("-")
+                        .map(|section| section.parse::<u32>().unwrap())
+                        .collect_tuple::<(u32, u32)>()
+                        .unwrap()
+                })
+                .collect_tuple::<(_, _)>()
+                .unwrap();
+            (bounds.0 .0 <= bounds.1 .0 && bounds.0 .1 >= bounds.1 .1)
+                || (bounds.0 .0 >= bounds.1 .0 && bounds.0 .1 <= bounds.1 .1)
+        })
+        .filter(|overlapping| *overlapping)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::num_overlapping_assignments;
+
+    #[test]
+    fn example() {
+        assert_eq!(2, num_overlapping_assignments("example.txt"))
+    }
+}
